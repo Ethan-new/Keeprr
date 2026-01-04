6593d6bdd279a3f874df9f1f303d989d2578a183
Ethan Um <ethanu.email@gmail.com>
2026-01-04 11:22:58 -0500

    Initial Commit

---
+++ Keeprr/KeeprrApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeeprrApp.swift
+//  Keeprr
+//
+//  Created by Ethan Um on 2026-01-04.
+//
+
+import SwiftUI
+
+@main
+struct KeeprrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KeeprrTests/KeeprrTests.swift
@@ -0,0 +1,17 @@
+//
+//  KeeprrTests.swift
+//  KeeprrTests
+//
+//  Created by Ethan Um on 2026-01-04.
+//
+
+import Testing
+@testable import Keeprr
+
+struct KeeprrTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
